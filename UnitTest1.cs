b2e9e172c7f0d401cc020f605ade2b09c9a5e11c
rchavez2583 <77551911+rchavez2583@users.noreply.github.com>
2021-02-06 09:42:27 -0700

    Add files via upload
    Add unitTest

---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnitTESTPROJECT11
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanUserBeAttacked_IfItsAPointer_ReturnsTrue()
+        {
+            //Arrange
+            MyClass class = new MyClass();
+
+
+        User newUser = new User();
+        newUser.IsPointer = true;
+           newUser.UserName = "Rene";
+
+            Clan newClan = new Clan();
+        newClan.ClanName = "Soluctions";
+            newUser.ClanInfo = newClan;
+            class.CurrentUser = newUser;
+            //Act
+            
+            User newUser = new User();
+        newUser.IsPointer = true;
+              newUser.UserName = "Jonh Doe";
+
+            Clan newUserClan = new Clan();
+        newUserClan.ClanName = "SU guys"
+
+            newUser.ClanInfo
+            bool canItBeAPointer = class.CanUserBeAttacked();
+
+        //Assert
+        Assert.ItTrue(canItBeAPointer);
+         }
+    }
+
+}
+
+             
